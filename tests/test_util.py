0c7575020a84b84857e6064094ef9687e6e3b44f
Vasily Kuznetsov <kvas.it@gmail.com>
2014-02-22 17:44:32 +0100

    Added bfs_probe and some unit tests for it.

---
+++ tests/test_util.py
@@ -0,0 +1,119 @@
+"""
+Tests for the util module.
+"""
+
+import pytest
+import sys
+import os
+
+bn = os.path.basename
+sys.path.append(bn(bn(__file__)))
+
+from util import TronGrid
+
+
+@pytest.fixture
+def tg():
+    return TronGrid()
+
+
+def test_constructor(tg):
+    assert tg.grid.count(0) == 600
+
+
+def test_bfs_fill(tg):
+    tg.bfs_fill(42, [0])
+
+    assert tg.grid.count(0) == 0
+    assert tg.grid.count(42) == 600
+
+
+def test_bfs_probe_center(tg):
+    res = tg.bfs_probe(tg.coords2index(15, 10))
+
+    assert res.closest_obstacle_d == 10
+    assert res.closest_obstacle == -1
+    assert res.max_distance == 25
+    assert res.empty_count == 599  # start point is never counted
+    assert res.values == {-1}
+
+
+def test_bsf_probe_objs(tg):
+    tg.put(10, 8, 1)
+    tg.put(5, 19, 2)
+
+    res = tg.bfs_probe(tg.coords2index(15, 10))
+
+    assert res.closest_obstacle_d == 7
+    assert res.closest_obstacle == 1
+    assert res.max_distance == 25
+    assert res.empty_count == 597  # subtract two points and start point
+    assert res.values == {1, 2, -1}
+
+
+@pytest.fixture
+def tg_box1(tg):
+    """Field with the tail of tron #1 going around the middle."""
+    for x in xrange(5, 25):
+        tg.put(x, 5, 1)
+        tg.put(x, 15, 1)
+    for y in xrange(5, 15):
+        tg.put(5, y, 1)
+        tg.put(25, y, 1)
+    return tg
+
+
+@pytest.fixture
+def tg_box1_in(tg_box1):
+    """Field with the tron #1 in the middle inside of its own tail."""
+    tg_box1.put(6, 6, 5)
+    return tg_box1
+
+
+@pytest.fixture
+def tg_box1_out(tg_box1):
+    """Field with the tron #1 in the middle just outside of its own tail."""
+    tg_box1.put(5, 4, 5)
+    return tg_box1
+
+
+def test_bsf_probe_box1(tg_box1):
+    res = tg_box1.bfs_probe(tg_box1.coords2index(15, 10))
+
+    assert res.closest_obstacle_d == 5
+    assert res.closest_obstacle == 1
+    assert res.max_distance == 13
+    assert res.empty_count == (9 * 19) - 1
+    assert res.values == {1}
+
+
+def test_bsf_probe_box1_in(tg_box1_in):
+    res = tg_box1_in.bfs_probe(tg_box1_in.coords2index(15, 10))
+
+    assert res.closest_obstacle_d == 5
+    assert res.closest_obstacle == 1
+    assert res.max_distance == 13
+    assert res.empty_count == (9 * 19) - 2
+    assert res.values == {1, 5}
+
+
+def test_bsf_probe_box1_out(tg_box1_out):
+    res = tg_box1_out.bfs_probe(tg_box1_out.coords2index(15, 10))
+
+    assert res.closest_obstacle_d == 5
+    assert res.closest_obstacle == 1
+    assert res.max_distance == 13
+    assert res.empty_count == (9 * 19) - 1
+    assert res.values == {1}
+
+
+def test_bfs_probe_pois(tg_box1):
+    c2i = tg_box1.coords2index
+    right = c2i(16, 10)
+    left = c2i(14, 10)
+    up = c2i(15, 9)
+    out = c2i(1, 1)
+
+    res = tg_box1.bfs_probe(c2i(15, 10), pois={right, left, up, out})
+
+    assert res.pois_reached == {left, right, up}
--- util.py
+++ util.py
@@ -1,23 +1,48 @@
-# Utils for the Tron Battle
+"""
+Utils for the Tron Battle.
+"""
 
 from array import array
-from copy import deepcopy
+from copy import deepcopy, copy
+
+
+class ProbeResult(object):
+    """Result of a probe."""
+
+    def __init__(self, closest_obstacle_d, closest_obstacle, steps,
+            empty_count, values, pois_reached):
+        self.closest_obstacle_d = closest_obstacle_d
+        self.closest_obstacle = closest_obstacle
+        self.max_distance = steps
+        self.empty_count = empty_count
+        self.values = values
+        self.pois_reached = pois_reached
 
 
 class TronGrid(object):
 
-    # Coordinates are mapped to indices like this:
-    #
-    #     idx = x + (y << 6)
-    #
-    # Then:
-    #
-    #     x = idx & 63
-    #     y = idx >> 6
-    #
-    # The size of the grid is 30 x 20 with some padding to avoid overflows with
-    # reasonably small moves. Initially the grid is filled with 0s for empty
-    # cells and -1s for walls.
+    """Data structure for the field of the tron battle.
+
+    Coordinates are mapped to indices like this:
+
+        idx = x + (y << 6)
+
+    Then:
+
+        x = idx & 63
+        y = idx >> 6
+
+    The size of the grid is 30 x 20 with some padding to avoid overflows with
+    reasonably small moves. Initially the grid is filled with 0s for empty
+    cells and -1s for walls.
+
+    The values in the grid are signed integers. Assigned values are:
+
+        * -1 wall
+        * 0 empty space
+        * 1-4 body of tron #1-4
+        * 5-8 head of tron #1-4
+    """
 
     DIRECTIONS = {
             'UP': -64,
@@ -52,6 +77,12 @@ class TronGrid(object):
     def index2coords(idx):
         return idx & 63, idx >> 6
 
+    def put(self, x, y, value):
+        self.grid[self.coords2index(x, y)] = value
+
+    def get(self, x, y):
+        return self.grid[self.coords2index(x, y)]
+
     def replace(self, src, dst):
         for i, value in enumerate(self.grid):
             if value == src:
@@ -75,4 +106,54 @@ class TronGrid(object):
         # print new_origins
         # raw_input()
 
+    def bfs_probe(self, start_idx, pois={}, limit=None, empty=0):
+        """See how far we can get from the ``start_idx``.
 
+        Returns the information about the surroundings of that point:
+
+            * Distance and values of the closest obstacle,
+            * Max distance walked,
+            * Number of passed positions that are empty,
+            * List of values encountered,
+            * List of POIs reached (taken out of ``pois`` set).
+
+        If ``limit`` is specified, don't probe beyond that many steps.
+        """
+        directions = self.DIRECTIONS.values()
+        grid = copy(self.grid)  # This doesn't change the grid.
+        origins = [start_idx]
+        steps = -1
+        empty_count = 0
+        pois_left = set(pois)
+        pois_reached = set()
+        values = set()
+        closest_obstacle = None
+        closest_obstacle_d = None
+        marker = 32000
+
+        while origins:
+            steps += 1
+            if limit is not None and steps >= limit:
+                break
+            new_origins = []
+            for origin in origins:
+                for d in directions:
+                    idx = origin + d
+                    if idx == start_idx: continue
+                    value = grid[idx]
+                    if value == empty:
+                        grid[idx] = marker
+                        new_origins.append(idx)
+                        empty_count += 1
+                    elif value != marker:
+                        if closest_obstacle is None:
+                            closest_obstacle = value
+                            closest_obstacle_d = steps + 1
+                        values.add(value)
+                    if idx in pois_left:
+                        pois_reached.add(idx)
+                        pois_left.remove(idx)
+            origins = new_origins
+
+        return ProbeResult(closest_obstacle_d, closest_obstacle, steps,
+                empty_count, values, pois_reached)
