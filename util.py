4944b979ac1a0deede550656d21222b22ad83b8c
Vasily Kuznetsov <kvas.it@gmail.com>
2014-02-22 15:37:00 +0100

    Breadth first search and a couple of tests. Performance on the empty field: ~1ms per search.

--- util.py
+++ util.py
@@ -18,8 +18,27 @@ class TronGrid(object):
     # reasonably small moves. Initially the grid is filled with 0s for empty
     # cells and -1s for walls.
 
+    DIRECTIONS = {
+            'UP': -64,
+            'DOWN': 64,
+            'LEFT': -1,
+            'RIGHT': 1
+    }
+
     def __init__(self):
         self.grid = array('h', ([0] * 30 + [-1] * 34) * 20 + [-1] * 128)
+
+    def __str__(self):
+        return '\n'.join(
+                ('{: >-4d} ' * 30).format(*self.grid[start:start + 30])
+                for start in range(0, 64 * 20, 64)
+        )
+
+    def __getitem__(self, idx):
+        return self.grid[idx]
+
+    def __setitem__(self, idx, value):
+        self.grid[idx] = value
 
     @staticmethod
     def coords2index(x, y):
@@ -30,7 +49,33 @@ class TronGrid(object):
         return idx & 63, idx >> 6
 
     def replace(self, src, dst):
-        for i in xrange(len(self.grid)):
-            if self.grid[i] == src:
+        for i, value in enumerate(self.grid):
+            if value == src:
                 self.grid[i] = dst
 
+    def bfs_fill(self, value, origins, empty=0):
+        new_origins = []
+        for origin in origins:
+            for d in self.DIRECTIONS.values():
+                idx = origin + d
+                if self.grid[idx] == empty:
+                    self.grid[idx] = value
+                    new_origins.append(idx)
+        if new_origins:
+            self.bfs_fill(value, new_origins, empty)
+
+
+def test():
+    t = TronGrid()
+    t.bfs_fill(42, [t.coords2index(5, 10)])
+    return t
+
+
+def test2():
+    t = TronGrid()
+    for y in xrange(0, 19):
+        idx1 = t.coords2index(10, y)
+        idx2 = t.coords2index(20, 19 - y)
+        t[idx1] = t[idx2] = 1
+    t.bfs_fill(42, [t.coords2index(5, 10)])
+    return t
