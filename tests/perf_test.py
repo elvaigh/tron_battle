4d76ceaff4f0188530001f078d7303395644aad7
Vasily Kuznetsov <kvas.it@gmail.com>
2014-02-25 14:54:37 +0100

    More performance tests for ray probe.

--- tests/perf_test.py
+++ tests/perf_test.py
@@ -13,10 +13,10 @@ sys.path.append(dn(ap(__file__)))
 from test_grid import tg, tg_box1
 
 
-TEMPLATE = '{name} ({count} times): {msecs:.0f} ms ({once:.3f} ms on average)'
+TEMPLATE = '{name}\n\tx {count} = {msecs:.3f} ms, average = {once:.3f} {unit}'
 
 
-def timed(times):
+def timed(times, in_times=1):
     def timed_decorator(function):
         @functools.wraps(function)
         def timed_func():
@@ -25,9 +25,16 @@ def timed(times):
                 function()
             end = time.time()
             msecs = (end - start) * 1000.0
-            once = msecs / times
-            print TEMPLATE.format(name=function.__doc__ or function.__name__,
-                    count=times, msecs=msecs, once=once)
+            once = msecs / times / in_times
+            unit = 'ms'
+            if once < 1:
+                once *= 1000
+                unit = '\xc2\xb5s'
+            name = function.__doc__ or function.__name__
+            if name.endswith('.'):
+                name = name[:-1] + ':'
+            print TEMPLATE.format(name=name, count=times * in_times,
+                    msecs=msecs, once=once, unit=unit)
         return timed_func
     return timed_decorator
 
@@ -67,7 +74,7 @@ def bfs_fill3():
     return t
 
 
-@timed(30)
+@timed(30, 100)
 def replace600_100():
     """Replace a value that has 600 positions in the grid."""
     t = tg()
@@ -76,7 +83,7 @@ def replace600_100():
     return t
 
 
-@timed(30)
+@timed(30, 100)
 def replace1_100():
     """Replace a value that has 1 position in the grid."""
     t = tg()
@@ -86,9 +93,9 @@ def replace1_100():
     return t
 
 
-@timed(50)
+@timed(50, 100)
 def copy_100():
-    """Copy the whole grid 100 times."""
+    """Copy the whole grid."""
     t = tg()
     for i in xrange(100):
         t1 = t.copy()
@@ -97,39 +104,55 @@ def copy_100():
 
 @timed(500)
 def bfs_probe_empty():
-    """Probe in the empty grid from the center."""
+    """BFS probe in the empty grid from the center."""
     t = tg()
     t.bfs_probe(t.coords2index(15, 10))
 
 
 @timed(500)
 def bfs_probe_box():
-    """Probe from the box in the center."""
+    """BFS probe from the box in the center."""
     t = tg_box1(tg())
     t.bfs_probe(t.coords2index(15, 10))
 
 
 @timed(500)
 def bfs_probe_limit10():
-    """Probe with the limit of 10."""
+    """BFS probe with the limit of 10."""
     t = tg()
     t.bfs_probe(t.coords2index(15, 10), limit=10)
 
 
-@timed(100)
+@timed(50, 100)
 def ray_probe_empty0_100():
-    """Ray probe 100 times from the center to the right width = 0."""
+    """Ray probe from the center, width = 0."""
     t = tg()
     for i in xrange(100):
         t.ray_probe(t.coords2index(15, 10), t.DIRECTIONS['RIGHT'])
 
 
-@timed(100)
+@timed(50, 100)
 def ray_probe_empty5_100():
-    """Ray probe 100 times from the center to the right width = 5."""
+    """Ray probe from the center, width = 5."""
     t = tg()
     for i in xrange(100):
         t.ray_probe(t.coords2index(15, 10), t.DIRECTIONS['RIGHT'], 5)
+
+
+@timed(50, 100)
+def ray_probe_box10_100():
+    """Ray probe inside the box, width = 10."""
+    t = tg_box1(tg())
+    for i in xrange(100):
+        t.ray_probe(t.coords2index(15, 10), t.DIRECTIONS['RIGHT'], 10)
+
+
+@timed(50, 100)
+def ray_probe_l5_100():
+    """Ray probe inside the box, width = 10, limit=5."""
+    t = tg()
+    for i in xrange(100):
+        t.ray_probe(t.coords2index(15, 10), t.DIRECTIONS['RIGHT'], 10, limit=5)
 
 
 if __name__ == '__main__':
@@ -143,3 +166,5 @@ if __name__ == '__main__':
     bfs_probe_limit10()
     ray_probe_empty0_100()
     ray_probe_empty5_100()
+    ray_probe_box10_100()
+    ray_probe_l5_100()
